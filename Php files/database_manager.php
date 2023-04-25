2a9571d69f3fb41c9c2ef71b71f5e091a93c3a73
Hexagon95 <vazul95@gmail.com>
2023-04-25 08:56:07 +0200

    Php files included

---
+++ Php files/database_manager.php
@@ -0,0 +1,45 @@
+<?php
+class DatabaseManager{
+    // ---------- <Variables [1]> ----- ---------- ---------- ---------- ---------- ---------- ---------- ----------    
+    private $pdoServer =   "localhost";
+    private $pdoDatabase = "mosaic";
+    //private $pdoDatabase = "mosaic_test";
+    private $pdoUser =     "app";
+    private $pdoPassword = "Dh!Flmn2J6uJ";
+    private $conn;
+    private $data;
+        public function getData(){return $this->data;
+    }
+    
+    // ---------- <Constructors> ------ ---------- ---------- ---------- ---------- ---------- ---------- ----------
+    function __construct($queryString, $parameters = []){        
+        $this->_connect();
+        $this->_executeQuery($queryString, $parameters);
+    }
+
+    // ---------- <Methods [1]> ------- ---------- ---------- ---------- ---------- ---------- ---------- ----------
+    private function _connect(){
+        try {            
+            $this->conn = new PDO("sqlsrv:Server=$this->pdoServer;Database=$this->pdoDatabase;", $this->pdoUser, $this->pdoPassword);            
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        catch (\Throwable $th) {            
+            echo json_encode(array(['Connection failed' => $th->getMessage()]));
+        }
+    }
+
+    private function _executeQuery($queryString, $parameters){
+        try {           
+            $sqlQuery = $this->conn->prepare($queryString);
+            $sqlQuery->execute($parameters);
+            try {
+                $sqlQuery->setFetchMode(PDO::FETCH_ASSOC);
+                $this->data = $sqlQuery->fetchAll();
+            }
+            catch (\Throwable $th) {}
+        }
+        catch (\Throwable $th) {
+            $this->data = json_encode(array(['Execution failed' => $th->getMessage()]));
+        }
+    }
+}
---
+++ Php files/tasks/task_login.php
@@ -0,0 +1,46 @@
+<?php
+class Task{
+    // ---------- <Variables [1]> ----- ---------- ---------- ---------- ---------- ---------- ---------- ----------
+    private $sqlCommand;
+    private $databaseManager;
+    private $request;
+    private $result;
+        public function getResult(){return $this->result;
+    }
+
+    // ---------- <Constructors> ------ ---------- ---------- ---------- ---------- ---------- ---------- ----------
+    function __construct(){       
+        $this->_inizialite();
+        $this->_checkID();
+    }
+
+    // ---------- <Methods [1]> ------- ---------- ---------- ---------- ---------- ---------- ---------- ----------
+    private function _inizialite(){
+        $this->request =            json_decode(file_get_contents('php://input'), true);
+        $this->sqlCommand =         new SqlCommand();
+        $this->databaseManager =    new DatabaseManager($this->sqlCommand->select_tabletLista(), [
+            'eszkoz_id' =>      $this->request['eszkoz_id']
+        ]);
+        $this->result = array([
+            'error' =>          "Az eszköz nincs dolgozóhoz rendelve! (".$this->request['eszkoz_id'].")",
+            'dolgozo_nev' =>    'Ismeretlen'
+        ]);
+    }
+
+    private function _checkID(){
+        $isMatch = false;
+        foreach ($this->databaseManager->getData() as $value) {
+            if($value['Eszkoz_id'] == $this->request['eszkoz_id']){
+                $isMatch = true;
+                if($value['dolgozo_kod'] > 0) $this->result = array(
+                    ['error' => '', 'dolgozo_nev' => $value['dolgozo_nev']],
+                    $value
+                );                
+                break;
+            }
+        }
+        if(!$isMatch) new DatabaseManager($this->sqlCommand->exec_tabletFelvitel(), [            
+            'eszkoz_id' => $this->request['eszkoz_id']
+        ]);
+    }
+}
